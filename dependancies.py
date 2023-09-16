95950a3561e707cdc4e6f96d56a13bd51ee1a193
Anurag Kakade <91648514+anuragkakade0@users.noreply.github.com>
2023-09-16 11:36:01 +0530

    dependancies.py

--- dependancies.py
+++ dependancies.py
@@ -87,7 +87,7 @@ def validate_username(username):
 
   #  pattern = "[A-Za-z0-9]+[.-_])*[A-Za-z0-9]*$"
    # pattern = "^[a-zA-Z0-9]*$"
-    pattern = "^[A-Za-z][A-Za-z0-9_]{3,29}$"
+    pattern = "^[A-Za-z0-9]{3,29}$"
 
     if re.match(pattern, username):
         return True
